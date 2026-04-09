b8297d13a20e3f467c48a378626da2e2aed7030e
aldoht <aldofernandoht@gmail.com>
2026-04-09 01:02:36 -0600

    fix: remove default menu option

--- milky/app/page.tsx
+++ milky/app/page.tsx
@@ -5,7 +5,7 @@ import SideBar from "../components/SideBar/SideBar";
 import MainContent from "../components/MainContent/MainContent";
 
 export default function Home() {
-  const [active, setActive] = useState("Submit report");
+  const [active, setActive] = useState("");
   
   return (
     <div className="grid grid-cols-4 grid-rows-10 bg-[#D8E4FF] h-screen w-screen">
