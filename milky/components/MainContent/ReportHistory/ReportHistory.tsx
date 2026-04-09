2a3145ea36ae2bb0e2340126b39c6a4223b39fe1
aldoht <aldofernandoht@gmail.com>
2026-04-09 01:07:51 -0600

    fix: make history scrollable if container is overflown

--- milky/components/MainContent/ReportHistory/ReportHistory.tsx
+++ milky/components/MainContent/ReportHistory/ReportHistory.tsx
@@ -5,10 +5,14 @@ export default function ReportHistory() {
     { id: 1, title: "Payment processed yet order not received", date: "2023-01-01", status: "Pending" },
     { id: 2, title: "Order has not been received", date: "2023-01-02", status: "Completed" },
     { id: 3, title: "Wrong shipping address", date: "2023-01-03", status: "Pending" },
+    { id: 4, title: "Payment processed yet order not received", date: "2023-01-01", status: "Pending" },
+    { id: 5, title: "Order has not been received", date: "2023-01-02", status: "Completed" },
+    { id: 6, title: "Wrong shipping address", date: "2023-01-03", status: "Pending" },
+    { id: 7, title: "Wrong shipping address", date: "2023-01-03", status: "Pending" },
   ].reverse();
 
   return (
-    <div className="space-y-2">
+    <div className="space-y-2 max-h-full overflow-scroll">
       {reports.map((report) => (
         <Report
           key={report.id}
